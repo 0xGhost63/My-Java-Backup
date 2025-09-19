0c89764bd6d3cedc9001021998eead6d87cc0b61
0xGhost63 <sp25-bse-118@cuilahore.edu.pk>
2025-09-19 05:09:05 +0500

    Week 1-3 DONE

--- ClassWork/Lab Tasks/Week 1/W1T4.java
+++ ClassWork/Lab Tasks/Week 1/W1T4.java
@@ -1,5 +1,3 @@
-// NOTE: MINOR DEBUG REQUIRED SOME FUNCTIONS DON'T WORK PROPERLY (CHOICE 4 & 5) + the cursor should move to the next line when a 
-// Case executes successfully :)
 import java.util.Scanner;
 class Rectangle
 {
@@ -7,60 +5,55 @@ class Rectangle
     double width;
     String colour;
     
-
     Rectangle(double len_of_side,double width,String colour)
     {
         this.len_of_side=len_of_side;
         this.width=width;
         this.colour=colour;
+    }
 
-    }
-    double getArea(double len_of_side,double width)
+    double getArea()
     {
-        double area;
-        area=this.len_of_side*this.width;
-        return area;
+        return this.len_of_side*this.width;
     }
-    double getPerimeter(double len_of_side,double width)
+
+    double getPerimeter()
     {
-        double perimeter;
-        double multiply;;
-        multiply=(this.len_of_side+this.width);
-        perimeter=multiply*2;
-        return perimeter;
+        return 2*(this.len_of_side+this.width);
     }
-    void isSquare(double len_of_side,double width){
+
+    void isSquare()
+    {
         if (this.len_of_side==this.width)
         {
             System.out.println("The specific rectangle is also a Square !");
-
         }
         else
         {
             System.out.println("This Rectangle is not a square :(");
         }
     }
+
     void change(double len_of_side,double width,String colour)
     {
         this.len_of_side=len_of_side;
         this.width=width;
         this.colour=colour;
     }
-    void displayInfo(double len_of_side,double width,String colour)
-    {
-        double area,perimeter;
-        area=getArea(len_of_side,width);
-        perimeter=getPerimeter(len_of_side, width);
-        System.out.printf("The length of the each side of the rectangle is : %.2f cm ",len_of_side);
-        System.out.printf("The width of the rectangle is : 2f% cm",width);
-        System.out.printf("The area of the Rectangle is : %.2f cm",area);
-        System.out.printf("The Perimeter of the rectangle is : %.2f cm ",perimeter);
-        System.out.println("The colour of the Rectangle is : "+colour);
-        isSquare(len_of_side, width);
-    }
-    
 
+    void displayInfo()
+    {
+        double area=getArea();
+        double perimeter=getPerimeter();
+        System.out.printf("The length of each side of the rectangle is : %.2f cm\n",this.len_of_side);
+        System.out.printf("The width of the rectangle is : %.2f cm\n",this.width);
+        System.out.printf("The area of the Rectangle is : %.2f cm\n",area);
+        System.out.printf("The Perimeter of the rectangle is : %.2f cm\n",perimeter);
+        System.out.println("The colour of the Rectangle is : "+this.colour);
+        isSquare();
+    }
 }
+
 public class W1T4 
 {
     public static void main(String[] args) 
@@ -68,13 +61,15 @@ public class W1T4
         double len_of_side,width;
         String colour;
         Scanner sc= new Scanner(System.in);
-        System.out.printf("Enter the length of the Each side of the Rectangle : ");
+
+        System.out.print("Enter the length of the Each side of the Rectangle : ");
         len_of_side=sc.nextDouble();
         System.out.print("Enter the width of the rectangle : ");
         width=sc.nextDouble();
-        sc.nextLine();
+        sc.nextLine(); // consume leftover newline
         System.out.print("Enter the colour of the Rectangle : ");
         colour=sc.nextLine();
+
         Rectangle R1=new Rectangle(len_of_side,width,colour);
 
         System.out.println("\n===Available Options====");
@@ -85,10 +80,12 @@ public class W1T4
         System.out.println("4-Change the dimensions of the Rectangle");
         System.out.println("5-Display all the info of the Rectangle :)");
         System.out.println("0-EXIT");
+
         while (true) 
         {
-            int choice;
-            choice=sc.nextInt();
+            int choice=sc.nextInt();
+            sc.nextLine(); // consume newline after int input
+
             if (choice==0)
             {
                 System.out.println("Have a nice day :)");
@@ -96,47 +93,40 @@ public class W1T4
             }
             else if(choice==1)
             {
-                double temp;
-                temp=R1.getArea(len_of_side,width);
-                System.out.printf("The area of the Rectangle is : %.2f cm",temp);
+                System.out.printf("The area of the Rectangle is : %.2f cm\n",R1.getArea());
             }
             else if (choice==2)
             {
-                double temp;
-                temp=R1.getPerimeter(len_of_side, width);
-                System.out.printf("The perimeter of the Rectangle is : %.2f cm ",temp);
+                System.out.printf("The perimeter of the Rectangle is : %.2f cm\n",R1.getPerimeter());
             }
             else if(choice==3)
             {
-                R1.isSquare(len_of_side, width);
-
+                R1.isSquare();
             }
             else if(choice==4)
             {
                 double length,motai;
                 String rung;
+
                 System.out.print("Enter the new length of each side of the Rectangle : ");
                 length=sc.nextDouble();
                 System.out.print("Enter the new Width of the Rectangle : ");
                 motai=sc.nextDouble();
+                sc.nextLine(); // consume newline here
                 System.out.print("Enter the new colour of the Rectangle : ");
                 rung=sc.nextLine();
-                R1.change(length, motai, rung);
-                System.out.println("Dimensions and colour changed successfully ");
 
+                R1.change(length, motai, rung);
+                System.out.println("Dimensions and colour changed successfully\n");
             }
             else if (choice==5)
             {
-                R1.displayInfo(len_of_side, width,colour);
+                R1.displayInfo();
             }
             else
             {
                 System.out.println("Invalid Operation Selected please try again : ");
             }
         }
-
-
-        
     }
-    
 }
