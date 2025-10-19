d5d977ae524c7af2542e42dc7ab34d701683d918
0xGhost63 <sp25-bse-118@cuilahore.edu.pk>
2025-10-19 06:54:51 +0500

    Tax adjustments & Main class Left

--- ClassWork/Assignments/BankingSystem.java
+++ ClassWork/Assignments/BankingSystem.java
@@ -25,6 +25,9 @@
   These values are used to make realProfit() reflect realistic economic data.
 */
 import java.util.Scanner;
+import java.lang.classfile.instruction.ConstantInstruction.IntrinsicConstantInstruction;
+import java.security.KeyStore.Entry;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -159,34 +162,6 @@ class CurrentAccount extends Account
 
 
 }
-//Enum
-enum Category
-    {
-        //Savings Class
-        YOUNG_SAVER(6),
-        ADULT_SAVER(7),
-        SENIOR_CITIZEN_SAVER(8),
-        
-        //Investment Class
-        ONE_YEAR(0.10),
-        THREE_YEAR(0.12),
-        FIVE_YEAR(0.14);
-
-
-        //Attribute
-        private final double profitRate;
-
-        //Private Constructor that implicitily prevents the creation of the objects outside of my choice
-        Category(double profitRate)
-        {
-            this.profitRate = profitRate;
-        }  
-
-        public double getProfitRate()
-        {
-            return profitRate;
-        }
-    }
 
 // Savings Account
 class SavingsAccount extends Account 
@@ -248,9 +223,6 @@ class SavingsAccount extends Account
     }
 }
 
-
-
-
 // Investment Account
 class InvestmentAccount extends Account 
 {
@@ -275,21 +247,42 @@ class InvestmentAccount extends Account
             this.timeCategory=timeCategory.FIVE_YEAR;
         }
     }
+    //Only returns the total profit/returnings also fulfills the abstract class demand
+    @Override
     public double getTotalEarnings() 
     {
         double interestRate = this.timeCategory.getProfitRate(); 
         double myMoney = this.balance;
         double totalProfit = 0;
-        System.out.println("==== PROJECTED PROFIT OVER " + this.years + " YEARS ====");
-        for(int year = 0; year < this.years; year++)
+        for(int year=0;year<this.years;year++)
         {
             double profitThisYear = myMoney * interestRate;
             totalProfit += profitThisYear;                      
             myMoney += profitThisYear;
-
-            System.out.printf("Year %d: Profit: %.2f PKR Money for next year: %.2f PKR\n",(year + 1), profitThisYear, myMoney);
+            
         }
+        return totalProfit; 
+    }
 
+    //Prints the projected profit for the number of years as well
+    public double getTotalEarnings(boolean shouldPrint) 
+    {
+        double interestRate = this.timeCategory.getProfitRate(); 
+        double myMoney = this.balance;
+        double totalProfit = 0;
+        if(shouldPrint)
+        {
+            System.out.println("==== PROJECTED PROFIT OVER " + this.years + " YEARS ====");
+            for(int year = 0; year < this.years; year++)
+            {
+                double profitThisYear = myMoney * interestRate;
+                totalProfit += profitThisYear;                      
+                myMoney += profitThisYear;
+
+                System.out.printf("Year %d: Profit: %.2f PKR Money for next year: %.2f PKR\n",(year + 1), profitThisYear, myMoney);
+            }
+
+        }
         return totalProfit; 
     }
 
@@ -315,30 +308,205 @@ class InvestmentAccount extends Account
             System.out.printf("The account balance after withdrawing %.2f PKR is : %.2f PKR\n",totalWithdrawlCost, this.balance);
         }
     }
+    
+    //nessary to implement this function as it is the part of the abstraction class
+    public double realProfit() 
+    {
+        Map <String,Map<Integer,Double>> dataMap=InflationData.getInflationData();
+
+        String country=null;
+        int year=0;
+        double result=0;
+
+        //Iterating Over the HashMap
+        System.out.print("\nAvailable options of countries are : ");
+        for (String key : dataMap.keySet() )
+        {
+            System.out.print(key+"   ");
+        }
+
+        System.out.print("\nEnter the Country : ");
+        country=BankingSystem.sc.next();
+        while(!dataMap.containsKey(country))
+        {
+            System.out.print("Invalid Country...Try Again : ");
+            country=BankingSystem.sc.next();
+        }
+        System.out.print("Enter an year : ");
+        year=BankingSystem.sc.nextInt();
+        while(year<2019 || year>2022)
+        {
+            System.out.print("Please enter a valid year (2019-2022) : ");
+            year=BankingSystem.sc.nextInt();
+        }
+        result=realProfit(country,year);
+
+        return result;
+    }
+
     public double realProfit(String country, int year)
     {
-        // Here 179689 is Nisab
-        //0.126 is the Inflation-Rate
-        //Applies to all years
+
         double realisticProfit=0;
         double nominal_profit=this.getTotalEarnings();
         double Zakat=(this.isZakatApplicable && this.balance>179689) ? this.balance*0.025 : 0;
         double tax=(this.isFiler) ? 0.15*nominal_profit : nominal_profit*0.35 ;
-        // get inflation from HashMap instead of hardcoding
+        // getting inflation from HashMap 
         Map<String, Map<Integer, Double>> inflationMap = InflationData.getInflationData();
         double inflationRate = 0;
         if (inflationMap.containsKey(country) && inflationMap.get(country).containsKey(year)) 
         {
             inflationRate = inflationMap.get(country).get(year);
         }
-        double inflationLoss = nominal_profit * inflationRate;        realisticProfit=nominal_profit-(tax+Zakat+inflationLoss);
+        double inflationLoss = nominal_profit * inflationRate;        
+        realisticProfit=nominal_profit-(tax+Zakat+inflationLoss);
         return Math.round(realisticProfit * 100.0) / 100.0;
     }
 
+
+    public void bestInvestment(boolean isComparison)
+    {
+        if(isComparison)
+        {
+            System.out.println("\n====  PROFIT COMPARISON   ====");
+
+            //Retrieving the data from the Hashmap
+            Map <String,Map<Integer,Double>> dataMap = InflationData.getInflationData();
+
+            //Variables for comparison
+            String country_1=null,country_2=null;
+            int year_1=0,year_2=0,duration_1=0,duration_2=0;
+            double profit_1=0,profit_2=0;
+
+            //Iterating Over the HashMap
+            System.out.print("\nAvailable options of countries are : ");
+            for (String key : dataMap.keySet() )
+            {
+                System.out.print(key+"   ");
+            }
+
+
+
+            //Taking the Input
+            System.out.print("\nEnter the first country : ");
+            country_1=BankingSystem.sc.nextLine();
+            while(!dataMap.containsKey(country_1))
+            {
+                System.out.printf("Invalid country ! Please try again : ");
+                country_1=BankingSystem.sc.nextLine();
+            }
+            System.out.printf("Enter the year of investment in %s : ",country_1);
+            year_1=BankingSystem.sc.nextInt();
+            BankingSystem.sc.nextLine();
+
+            System.out.printf("Enter the second country : ");
+            country_2=BankingSystem.sc.nextLine();
+            while(!dataMap.containsKey(country_2))
+            {
+                System.out.printf("Invalid country ! Please try again : ");
+                country_2=BankingSystem.sc.nextLine();
+            }
+            System.out.printf("Enter the year of investment in %s : ",country_2);
+            year_2=BankingSystem.sc.nextInt();
+
+            //Calculations
+
+            profit_1=realProfit(country_1,year_1);
+            profit_2=realProfit(country_2,year_2);
+            System.out.println("STATISTICS : ");
+            System.out.printf("Profit in %s in %d is : %.2f PKR\n",country_1,year_1,profit_1);
+            System.out.printf("Profit in %s in year %d is : %.2f PKR\n",country_2,year_2,profit_2);
+            System.out.println("Hence : ");
+            if(profit_1>profit_2) 
+            {
+                System.out.printf("%s Is a better option for investment in year %d than %s in %d\n",country_1,year_1,country_2,year_2 );
+            }
+            else
+            {
+                System.out.printf("%s Is a better option for investment in year %d than %s in %d \n",country_2,year_2,country_1,year_1 );
+            }
+        }
+    }       
+
     public void bestInvestment()
     {
-        
+        // best year per country
+        Map<String, Map<Integer, Double>> dataMap = InflationData.getInflationData();
+        int sizeOfMap=dataMap.size();
+        ArrayList <String> countriresArray = new ArrayList<>();
+        ArrayList<Double> countriesProfit = new ArrayList<>();
+        ArrayList<Integer>bestTime = new ArrayList<>();
+
+        // overall best year
+        double overallHighestProfit = Double.MIN_VALUE;
+        String overallBestCountry = "";
+        int overallBestYear = 0;
+
+
+        //Which country is best for investment overall ??
+        //Which year is specifically the best ?
+        for(Map.Entry<String , Map <Integer,Double>> data : dataMap.entrySet())  
+        {
+            double sum=0;
+            double highestProfit=Double.MIN_VALUE;
+            int bestYear=0; // initialized
+            countriresArray.add(data.getKey());
+
+
+            Map<Integer, Double> innerMap = data.getValue();
+
+            for(Map.Entry <Integer,Double> innerData: innerMap.entrySet())
+            {
+                double profit = 0;
+                String country = data.getKey();
+                int year = innerData.getKey();
+                profit=realProfit(data.getKey(),innerData.getKey());
+
+                if(profit>highestProfit)
+                {
+                    highestProfit=profit;
+                    bestYear=innerData.getKey();
+                }
+                if (profit > overallHighestProfit)
+                {
+                    overallHighestProfit = profit;
+                    overallBestCountry = country;
+                    overallBestYear = year;
+                }
+
+                sum+=profit;
+            }
+            countriesProfit.add(sum);
+            bestTime.add(bestYear); // keep it here
+        }      
+
+        double countriesMax=0;
+        int countryNumber=0;
+        for (int i =0;i<countriesProfit.size();i++)
+        {
+            if(countriesProfit.get(i)>countriesMax)
+            {
+                countriesMax=countriesProfit.get(i);
+                countryNumber=i;
+
+            }
+        }
+
+        // Print results
+        System.out.println("\n===== BEST INVESTMENT REPORT =====");
+        System.out.printf("Best Country: %s\n", countriresArray.get(countryNumber));
+        System.out.printf("Total Potential Profit in %s: %.2f PKR\n", countriresArray.get(countryNumber), countriesProfit.get(countryNumber));
+        System.out.printf("Best Year to Invest in %s: %d\n", countriresArray.get(countryNumber), bestTime.get(countryNumber));
+
+        // Overall best year across all countries
+        System.out.println("\n=== OVERALL BEST INVESTMENT YEAR ===");
+        System.out.printf("Country: %s\n", overallBestCountry);
+        System.out.printf("Year: %d\n", overallBestYear);
+        System.out.printf("Profit in that year: %.2f PKR\n", overallHighestProfit);
+
     }
+
+
 
 
 
@@ -384,16 +552,68 @@ class InflationData
 
 
 }
+//Enum
+enum Category
+    {
+        //Savings Class
+        YOUNG_SAVER(6),
+        ADULT_SAVER(7),
+        SENIOR_CITIZEN_SAVER(8),
+        
+        //Investment Class
+        ONE_YEAR(0.10),
+        THREE_YEAR(0.12),
+        FIVE_YEAR(0.14);
+
+
+        //Attribute
+        private final double profitRate;
+
+        //Private Constructor that implicitily prevents the creation of the objects outside of my choice
+        Category(double profitRate)
+        {
+            this.profitRate = profitRate;
+        }  
+
+        public double getProfitRate()
+        {
+            return profitRate;
+        }
+    }
 
 // Main Class
-public class BankingSystem {
-    public static void main(String[] args) {
+public class BankingSystem 
+{
+    //Scanner Object Creation
+    public static final Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) 
+    {
+
         System.out.println("Hello World");
         System.out.println("=====    WELCOME!    =====");
+        // Sample accounts and summary
+
+        InvestmentAccount i1 = new InvestmentAccount(3, 204, "Ali", 40, 100000.0, true, true);
+        i1.bestInvestment();
+        double realProfit=i1.realProfit();
+        System.out.println("Real profit : "+realProfit);
+        realProfit=i1.realProfit("Pakistan",2020);
+        System.out.println("Real profit : "+realProfit);
+        
+        // i1.realProfit("Pakistan",2019);
+        // InvestmentAccount.bestInvestment();
+
+
+        sc.close();
+
+        // FORMAT THE AMOUNTS !
+
 
 
         
-        // // SavingsAccount s1 = new SavingsAccount(0, "Sannan", 10, 100, false, false);
+        // // SavingsAc        Map<String , Map<String,Integer> Data = new HashMap<>();
+        // Data=InflationData.getInflationData();
+        // System.out.println(Data);count s1 = new SavingsAccount(0, "Sannan", 10, 100, false, false);
         // InvestmentAccount i1 = new InvestmentAccount(3, 1, "Ali", 40, 100000, true, true);
 
         // double temp=i1.getTotalEarnings();
@@ -406,23 +626,67 @@ public class BankingSystem {
         //Account.getTotalProfitPaid() to get all the accounts
 
         // 2-
-        /*Capital Gain Tax: Your filer/non-filer tax logic is mostly correct; just make non-filer tax consistent between getTotalEarnings() and realProfit().
-
-        InvestmentAccount Profit: Right now it’s simple multiplication; needs compounding for multi-year plans.
-
-        Withdraw Tax: Surcharge & tax are fine; optional: could return boolean/exception instead of just printing.
-
-        deductZakat(): Works, just fix the printf formatting.
-
-        Category Enum: Works, but consider separating SavingsCategory vs InvestmentCategory for clarity.
-
-        getTotalProfit(): Works, can rename to getTotalProfitPaid for clarity.
-
-        Real Profit / Inflation: Use a Map<String, Map<Integer, Double>> for country/year inflation to make it flexible instead of hard-coded Pakistan.
-
-        Main/Test Class: Create 5–7 accounts of all types, print individual earnings & total profit.
-
-        Code Hygiene: Keep indentation clean, avoid printing inside getTotalEarnings() in production, and use meaningful variable names in loops./*
+        /*⚠️ Leftovers / Changes needed
+            
+        Capital gain tax fix
+            
+        Non-filer earnings tax for getTotalEarnings() should be 25% (currently 35%)
+            
+        Filer earnings tax remains 15% ✔
+            
+        Non-filer withdrawal tax → 4% (currently only InvestmentAccount implemented)
+            
+        Filer withdrawal tax → 2%
+            
+        Withdrawal tax for SavingsAccount
+            
+        Right now only InvestmentAccount applies it
+            
+        Needs to be implemented for SavingsAccount too
+            
+        Remove print statements from calculation methods
+            
+        getTotalEarnings() should return value only, no printing
+            
+        Move prints to main/test class
+            
+        Add 7 countries to inflation data
+            
+        Current: Pakistan, Japan, China
+            
+        Assignment wants: Pakistan, Japan, China, Lebanon + 3 others
+            
+        Inflation rates for 2018–2022 must be included
+            
+        Implement getRealProfitRatio()
+            
+        Returns actual profit percentage after tax, zakat, inflation
+            
+        Currently you return absolute realProfit amount
+            
+        Test class / main method updates
+            
+        Create 7 InvestmentAccount objects (2018, 5-year plan, different countries)
+            
+        Print each account’s earnings & real profit ratio
+            
+        Determine best country for long-term investment
+            
+        Consistency / minor fixes
+            
+        duration_1, duration_2 in bestInvestment(boolean) are unused → remove
+            
+        Variable naming (countriresArray typo) → fix
+            
+        Minor formatting issues in printf
+            
+        realProfit() in SavingsAccount doesn’t take country/year → optional to extend for uniformity
+            
+        Optional / bonus
+            
+        Ensure future inflation data can be added without code changes
+            
+        All earning accounts handle reinvestment via deposit(profit) (already works)*/
 
       
        
