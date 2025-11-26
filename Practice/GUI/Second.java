a4891cf84417a88442dab65c5566352d2e13bfa2
0xGhost63 <sp25-bse-118@cuilahore.edu.pk>
2025-11-27 03:23:28 +0500

    Started Learning GUI

--- Practice/GUI/Second.java
+++ Practice/GUI/Second.java
@@ -2,6 +2,9 @@ import javafx.application.Application;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -20,6 +23,22 @@ public class Second extends Application
     public void start(Stage myStage)
     {
         myStage.setTitle("Drawing :)");
+        Pane root = new Pane();
+        //TEXT
+        Text text = new Text();
+        text.setText("Hi from 0xGhost !! :)");
+        text.setFill(Color.RED);
+        text.setFont(Font.font("Verdana", FontWeight.BOLD, 30));
+        text.setX(30);
+        text.setY(50);
+        root.getChildren().add(text);
+
+        Line line = new Line();
+
+        Scene scene = new Scene(root,600,600,Color.LIGHTBLUE);
+
+
+        myStage.setScene(scene);
         myStage.show();
     }
 
