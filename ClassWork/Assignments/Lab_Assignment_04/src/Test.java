a581c76f88ce9fca1ad85a4384f23a8ca536a16c
0xGhost63 <sp25-bse-118@cuilahore.edu.pk>
2025-12-10 16:29:58 +0500

    Class Assignment # 04 Started

---
+++ ClassWork/Assignments/Lab_Assignment_04/src/Test.java
@@ -0,0 +1,28 @@
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Test extends Application 
+{
+    @Override
+    public void start(Stage primaryStage) 
+    {
+        Label label = new Label("JavaFX is running perfectly!");
+
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 250);
+
+        primaryStage.setTitle("JavaFX Test");
+        primaryStage.setScene(scene);
+        primaryStage.
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) 
+    {
+        launch(args);
+    }
+}
